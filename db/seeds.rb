6cbb632ad45c2ad81083aaeeff0d09059bf218f7
kaveesh sunassee <kaveesh.sunassee@gmail.com>
2021-09-03 14:26:16 +0400

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+puts "Starting seeds"
+kaveesh = User.new(first_name: "Kaveesh", last_name: "Sunassee", phone_number: 57141618)
+kaveesh.save!
+fabrice = User.new(first_name: "Fabrice", last_name: "Madre", phone_number: 12345678)
+fabrice.save!
+priyanka = User.new(first_name: "Priyanka", last_name: "Kallee", phone_number: 87654321)
+priyanka.save!
+puts "Successful seeds"
